5aeec4e027760c45004ade54b2806f7f3bd0f550
Lee-na-yun <happyin3333@nate.com>
2022-11-07 12:43:14 +0900

    aws 데이터베이스 생성 및 환경변수 설정 & 배포하기

--- src/main/java/site/metacoding/awsv4/domain/Book.java
+++ src/main/java/site/metacoding/awsv4/domain/Book.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Getter
+@Table(name = "book")
 @Entity
 public class Book {
 
